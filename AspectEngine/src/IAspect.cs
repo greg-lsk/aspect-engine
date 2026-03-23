ace64f4b2d7a05828bc4057ade6afc287740fbc4
Greg Liaskas <greg.liaskas@outlook.com>
2026-03-23 22:07:14 +0200

    update:: Seperated concern of 'IAspectMetadata' and 'IAspect'.
    -'IAspect<>' renamed to 'IAspectOn<>' and indicates the data the aspect will run on.
    -'IAspectMetadata' indicates the excecution.

--- AspectEngine/src/IAspect.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AspectEngine;
-
-public interface IAspectMetadata { }
-
-public interface IAspect<TData> : IAspectMetadata
-{
-    public void Run(in TData data);
-}
---
+++ AspectEngine/src/IAspectOn.cs
@@ -0,0 +1,11 @@
+﻿namespace AspectEngine;
+
+public interface IAspectMetadata 
+{
+    public void Run();
+}
+
+public interface IAspectOn<TData>
+{
+    public TData Context { get; }
+}
