679aa087f3dcb3fc5d8fca924a27667bfd52a29a
Greg Liaskas <greg.liaskas@outlook.com>
2026-03-23 13:27:13 +0200

    draft:: Demo project refactor.

--- _AspectDemo[temp]/Aspects/Logging/.sourceGenerated/EvaluationLogging.cs
+++ _AspectDemo[temp]/Aspects/Logging/.sourceGenerated/EvaluationLogging.cs
@@ -2,6 +2,7 @@
 
 internal readonly partial struct EvaluationLogging
 {
-    internal EvaluationLogging(Func<EvaluationLoggingDependencies> dependenciesFactory) => Dependencies = dependenciesFactory();
     public EvaluationLoggingDependencies Dependencies { get; }
+
+    internal EvaluationLogging(Func<EvaluationLoggingDependencies> dependenciesFactory) => Dependencies = dependenciesFactory();
 }
