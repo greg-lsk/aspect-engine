89cf647980302874cb3c38fb8c00b722501a8bc8
Greg Liaskas <greg.liaskas@outlook.com>
2026-03-23 11:41:50 +0200

    draft:: Demo project update

---
+++ _AspectDemo[temp]/Aspects/Logging/.sourceGenerated/EvaluationLoggingDependencies.sg.cs
@@ -0,0 +1,14 @@
+﻿using AspectEngine.DependencyInjection;
+
+
+namespace AspectDemo.Aspects.Logging;
+
+internal readonly struct EvaluationLoggingDependencies : IInjectableMetadata
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    private EvaluationLoggingDependencies(IServiceProvider serviceProvider) => _serviceProvider = serviceProvider;
+
+    public Microsoft.Extensions.Logging.ILogger Logger 
+        => (Microsoft.Extensions.Logging.ILogger) _serviceProvider.GetService(typeof(Microsoft.Extensions.Logging.ILogger));
+}
---
+++ _AspectDemo[temp]/Aspects/Logging/IEvaluationLoggingDependecies.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Logging;
+using AspectEngine.DependencyInjection;
+
+
+namespace AspectDemo.Aspects.Logging;
+
+[Injectable]
+public interface IEvaluationLoggingDependecies
+{
+    public ILogger Logger { get; }
+}
