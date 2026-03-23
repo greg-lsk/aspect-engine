e785d30a225c774efa6fd2efac7bf0a3f9c55a57
Greg Liaskas <greg.liaskas@outlook.com>
2026-03-23 22:03:54 +0200

    update:: 'AspectAttribute' introduced.

---
+++ AspectEngine/src/AspectAttribute.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+
+namespace AspectEngine;
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+public sealed class AspectAttribute<TAspect> : Attribute where TAspect : IAspectMetadata { }
