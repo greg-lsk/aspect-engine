e3b3cdbfe4e62740bd97d16ff4d0ef486ccc9322
Greg Liaskas <greg.liaskas@outlook.com>
2026-03-26 12:30:07 +0200

    Update:: Seperated aspect execution from dependency resolution.

--- AspectEngine/src/AspectHandler.cs
+++ AspectEngine/src/AspectHandler.cs
@@ -1,21 +1,9 @@
-﻿using System;
-using Microsoft.Extensions.DependencyInjection;
-
-
-namespace AspectEngine;
+﻿namespace AspectEngine;
 
 public class AspectHandler
 {
-    private readonly IServiceProvider _serviceProvider;
-
-
     public AspectExecutionEnvironment<TData> CreateEnvironment<TData>(TData data)
     {
         return AspectExecutionEnvironment<TData>.Create(data);
-    }
-
-    public TService ResolveDependency<TService>() where TService : notnull
-    {
-        return _serviceProvider.GetRequiredService<TService>();
     }
 }
---
+++ AspectEngine/src/DependencyInjection/Resolution.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace AspectEngine.DependencyInjection;
+
+public class Resolution
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public TService For<TService>() where TService : notnull
+    {
+        return _serviceProvider.GetRequiredService<TService>();
+    }
+}
