d175823a91e5b8420b93253b1a36ced911d16518
Greg Liaskas <greg.liaskas@outlook.com>
2026-04-03 23:28:52 +0300

    draft:: Trying ways to implement proxied-resolution.

--- AspectEngine/src/DependencyInjection/IProxiedResolution.cs
+++ AspectEngine/src/DependencyInjection/IProxiedResolution.cs
@@ -1,11 +1,60 @@
-﻿namespace AspectEngine.DependencyInjection;
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
 
-public interface IProxiedResolution<TContext, TResolved>
-{
-    public TResolved Create(in TContext context);
-}
+
+namespace AspectEngine.DependencyInjection;
+
 
 public interface IProxiedResolution<TResolved>
 {
     public TResolved Create();
+}
+
+public interface IProxiedResolution<TContext, TResolved>
+{
+    public TResolved Create();
+    
+    public Wrap<TContext, TResolved, IProxiedResolution<TContext, TResolved>> AsScoped(); 
+
+}
+
+
+public delegate IServiceScope ScopeFactory();
+public delegate IServiceProvider ProviderSource();
+public abstract class ProxiedResolutionBase
+{
+    protected readonly ScopeFactory _scopeFactory;
+    protected readonly ProviderSource _providerSource;
+
+    protected ProxiedResolutionBase(ScopeFactory scopeFactory, ProviderSource providerSource)
+    {
+        _scopeFactory = scopeFactory;
+        _providerSource = providerSource;
+    }
+}
+
+
+public class Wrap<TContext, TResolved, TFactory> : IDisposable, IProxiedResolution<TContext, TResolved>
+      where TFactory : IProxiedResolution<TContext, TResolved>
+{
+    private readonly IServiceScope _scope;
+    private readonly TFactory _factory;
+
+    public Wrap(IServiceScope scope, TFactory factory)
+    {
+        _scope = scope;
+        _factory = factory;
+    }
+
+    public Wrap<TContext, TResolved, IProxiedResolution<TContext, TResolved>> AsScoped()
+    {
+        return _factory.AsScoped();
+    }
+
+    public TResolved Create() => _factory.Create();
+
+    public void Dispose()
+    {
+        _scope.Dispose();
+    }
 }
