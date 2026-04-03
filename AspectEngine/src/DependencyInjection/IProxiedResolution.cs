f0ba3c0649b4514b28adafaa8f7816eb37632f0d
Greg Liaskas <greg.liaskas@outlook.com>
2026-04-03 08:45:30 +0300

    refactor:: Renamed 'Resolution.cs' to 'IProxiedResolution.cs'.

---
+++ AspectEngine/src/DependencyInjection/IProxiedResolution.cs
@@ -0,0 +1,11 @@
+﻿namespace AspectEngine.DependencyInjection;
+
+public interface IProxiedResolution<TContext, TResolved>
+{
+    public TResolved Create(in TContext context);
+}
+
+public interface IProxiedResolution<TResolved>
+{
+    public TResolved Create();
+}
--- AspectEngine/src/DependencyInjection/Resolution.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace AspectEngine.DependencyInjection;
-
-public interface IServiceFactory<TContext, TService>
-{
-    public TService Create(in TContext context);
-}
