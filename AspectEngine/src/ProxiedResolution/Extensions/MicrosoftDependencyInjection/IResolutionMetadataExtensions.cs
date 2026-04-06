2eddf938eeccc29133201d080378ff79de4e3837
Greg Liaskas <greg.liaskas@outlook.com>
2026-04-06 23:27:38 +0300

    update:: Removed 'IResolution' and 'IResolutionContext' types.

--- AspectEngine/src/ProxiedResolution/Extensions/MicrosoftDependencyInjection/IResolutionMetadataExtensions.cs
+++ AspectEngine/src/ProxiedResolution/Extensions/MicrosoftDependencyInjection/IResolutionMetadataExtensions.cs
@@ -8,6 +8,6 @@ public static class IResolutionMetadataExtensions
     public static T Materialize<T>(this IResolutionMetadata<T> resolutionMetadata, IServiceScope scope) where T : struct
     {
         var context = ResolutionContext<T>.Create(scope, resolutionMetadata);
-        return (resolutionMetadata as ICreator<T, ResolutionContext<T>>).Create(in context);
+        return (resolutionMetadata as IResolver<ResolutionContext<T>, T>).Resolve(in context);
     }
 }
--- AspectEngine/src/ProxiedResolution/IResolutionContextHolder.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace AspectEngine.ProxiedResolution;
-
-public interface IResolutionContextHolder<T, TContext> 
-    where T : struct
-    where TContext : struct, IResolutionContext<T>
-{
-    public TContext ResolutionContext { get; }
-}
