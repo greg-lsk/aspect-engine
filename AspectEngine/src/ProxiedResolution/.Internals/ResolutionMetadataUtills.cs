dbd475697d9bd5059df946c697caeb5f05494947
Greg Liaskas <greg.liaskas@outlook.com>
2026-04-08 23:02:24 +0300

    update:: Moved 'ResolutionSource' assignment logic to 'ResolutionSourceUtills.cs'.

--- AspectEngine/src/ProxiedResolution/.Internals/ResolutionMetadataUtills.cs
+++ AspectEngine/src/ProxiedResolution/.Internals/ResolutionMetadataUtills.cs
@@ -5,6 +5,10 @@ internal class ResolutionMetadataUtills : IResolutionMetadataUtills
     public ResolutionSource CreateResolutionSource<T>(IResolutionMetadata<T> resolutionMetadata, object? resolutionSource) 
         where T : struct
     {
-        return ResolutionSource.Create(resolutionMetadata, resolutionSource);
+        var source = resolutionSource is not null 
+            ? resolutionSource
+            : (resolutionMetadata as ResolutionMetadata<T>).HostingContainer;
+        return ResolutionSource.Create(source);
+    }
     }
 }
--- AspectEngine/src/ProxiedResolution/ResolutionSource.cs
+++ AspectEngine/src/ProxiedResolution/ResolutionSource.cs
@@ -9,13 +9,8 @@ public readonly struct ResolutionSource
     {
         Instance = resolutionSource;
     }
-    internal static ResolutionSource Create<T>(IResolutionMetadata<T> resolutionMetadata, object? resolutionSource) 
-        where T : struct
+    internal static ResolutionSource Create(object? resolutionSource)
     {
-        var source = resolutionSource is not null 
-            ? resolutionSource 
-            : (resolutionMetadata as ResolutionMetadata<T>).HostingContainer;
-
-        return new(source);
+        return new(resolutionSource);
     }
 }
